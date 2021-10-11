d0b3c538eeece1fc0a3c2d33fcab0ff6e960a1b1
tresor <trsordiambu2@gmail.com>
2021-10-11 15:50:18 +0200

    premiere modif

--- programme.py
+++ programme.py
@@ -12,8 +12,17 @@ def Nom_Stephane ():
 def Nom_Quentin():
     return "Roulet"
 
-def print_classe ():
+def Nom_diambu():
+    return "diambu"
+
+def Prenom_Tresor():
+    return "tresor"
+
+def print_classe():
+
     print (Prenom_Stephane(), " ", Nom_Stephane())
     print(Prenom_Quentin()," ",Nom_Quentin())
+    print(Prenom_Tresor(), " ", Nom_diambu())
+
 
 print_classe()
