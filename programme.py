116f5724068459b967bb379c15ebce5389d271e4
a_sabella@stu-digital-campus.fr <a_sabe>
2021-10-11 15:05:38 +0200

    ajout du programme py

--- programme.py
+++ programme.py
@@ -6,7 +6,18 @@ def Prenom_Stephane ():
 def Nom_Stephane ():
     return "Godin"
 
+def Prenom_Anthony ():
+    return "Anthony"
+
+def Nom_Anthony ():
+    return "Sabella"
+
 def print_classe ():
     print (Prenom_Stephane(), " ", Nom_Stephane())
+    print(Prenom_Anthony(), " ", Nom_Anthony())
+
+
+
+
 
 print_classe()
