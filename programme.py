ad3dfbcbc45cf56d68e169aff42cf83f3646d1d3
Leo Gou <leo_g92@hotmail.fr>
2021-10-11 15:18:36 +0200

    create leo functions

--- programme.py
+++ programme.py
@@ -6,14 +6,21 @@ def Prenom_Stephane ():
 def Prenom_Quentin():
     return "Quentin"
 
+def Prenom_Leo():
+    return "Léo"
+
 def Nom_Stephane ():
     return "Godin"
 
 def Nom_Quentin():
     return "Roulet"
 
+def Nom_Leo():
+    return "Gouaislin"
+
 def print_classe ():
     print (Prenom_Stephane(), " ", Nom_Stephane())
     print(Prenom_Quentin()," ",Nom_Quentin())
+    print(Prenom_Leo()," ",Nom_Leo())
 
 print_classe()
