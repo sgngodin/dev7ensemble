23e9e75e14616fff41697268aca6de7b2290e819
crocsg <34553036+crocsg@users.noreply.github.com>
2021-10-11 14:46:49 +0200

    stephane

---
+++ programme.py
@@ -0,0 +1,12 @@
+
+
+def Prenom_Stephane ():
+    return "Stephane"
+
+def Nom_Stephane ():
+    return "Godin"
+
+def print_classe ():
+    print (Prenom_Stephane(), " ", Nom_Stephane())
+
+print_classe()
