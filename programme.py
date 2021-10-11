506a8127bd57b659541057a04b72564ee71aec03
abc <abc@example.com>
2021-10-11 15:21:53 +0200

    Jessy

--- programme.py
+++ programme.py
@@ -6,7 +6,17 @@ def Prenom_Stephane ():
 def Nom_Stephane ():
     return "Godin"
 
+def Prenom_Jessy():
+    return "Jessy"
+
+def Nom_Jessy ():
+    return "Giteau"
+
 def print_classe ():
     print (Prenom_Stephane(), " ", Nom_Stephane())
 
-print_classe()
+def print_classe ():
+    print(Prenom_Jessy(), " ", Nom_Jessy())
+
+
+    print_classe()
